7cb62b7a1491e3f1904a98ba08f39d9c3cfe0aa7
Jaleel Adesina <jtutoff@gmail.com>
2020-11-04 15:21:23 +0100

    vue calendar  done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <AppHeader/>
   <div class="w-full flex">
-    <DcHeroes/>
+    <Calender/>
   </div>
 </template>
 
 <script>
   import AppHeader from './components/AppHeader';
-  import DcHeroes from './components/DcHeroes';
+  import Calender from './components/Calender';
 
   export default {
-    components: { AppHeader, DcHeroes },
+    components: { AppHeader, Calender },
   }
 </script>
